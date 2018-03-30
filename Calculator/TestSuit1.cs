ffb1be1438375ed98c0e4a492ac8e7748b9c41f6
Antal Nagy <antaln1985+attrecto@gmail.com>
2018-03-30 13:56:51 +0000

    commit6

--- Calculator/TestSuit1.cs
+++ Calculator/TestSuit1.cs
@@ -25,12 +25,22 @@ namespace Calculator
 
         public void Decimal()
         {
-            driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
-            driver.FindElement(By.LinkText("$$1")).Click();
-            driver.FindElement(By.LinkText("$$.")).Click();
-            driver.FindElement(By.LinkText("$$1")).Click();
-            string decimalValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div")).Text;
-            Assert.IsTrue(decimalValue.Contains("1.1"));
+          try
+            {
+
+                driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
+                driver.FindElement(By.LinkText("$$1")).Click();
+                driver.FindElement(By.LinkText("$$.")).Click();
+                driver.FindElement(By.LinkText("$$1")).Click();
+                string decimalValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div")).Text;
+                Assert.IsTrue(decimalValue.Contains("1.1"));
+                Console.WriteLine("Test No. 2 - Passed");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Test No. 2 - An error has occured.");
+            }
 
         }
 
@@ -40,13 +50,21 @@ namespace Calculator
 
         public void ClearAll()
         {
-            driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
-            driver.FindElement(By.LinkText("$$9")).Click();
-            driver.FindElement(By.LinkText("$$6")).Click();
-            driver.FindElement(By.LinkText("$$5")).Click();
-            driver.FindElement(By.XPath("//div[@id='main']/div/div/div[3]/div/div[8]")).Click();
-            string clearAllValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[4]/div/div/span[2]")).Text;
-            Assert.IsEmpty(clearAllValue);
+            try
+            {
+                driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
+                driver.FindElement(By.LinkText("$$9")).Click();
+                driver.FindElement(By.LinkText("$$6")).Click();
+                driver.FindElement(By.LinkText("$$5")).Click();
+                driver.FindElement(By.XPath("//div[@id='main']/div/div/div[3]/div/div[8]")).Click();
+                string clearAllValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[4]/div/div/span[2]")).Text;
+                Assert.IsEmpty(clearAllValue);
+                Console.WriteLine("Test No. 3 - Passed");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Test No. 3 - An error has occured.", e);
+            }
         }
 
         [Test]
@@ -55,13 +73,23 @@ namespace Calculator
 
         public void Undo()
         {
-            driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
-            driver.FindElement(By.LinkText("$$5")).Click();
-            driver.FindElement(By.XPath("//div[@id='main']/div/div/div[3]/div/div[8]")).Click();
-            driver.FindElement(By.XPath("//div[@id='main']/div/div/div[3]/div/div[5]/i")).Click();
-            driver.FindElement(By.XPath("//div[@id='main']/div/div/div[3]/div/div[6]/i")).Click();
-            string undoValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[4]/div/div")).Text;
-            Assert.IsEmpty(undoValue);
+            try
+            {
+
+                driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
+                driver.FindElement(By.LinkText("$$5")).Click();
+                driver.FindElement(By.XPath("//div[@id='main']/div/div/div[3]/div/div[8]")).Click();
+                driver.FindElement(By.XPath("//div[@id='main']/div/div/div[3]/div/div[5]/i")).Click();
+                driver.FindElement(By.XPath("//div[@id='main']/div/div/div[3]/div/div[6]/i")).Click();
+                string undoValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[4]/div/div")).Text;
+                Assert.IsEmpty(undoValue);
+                Console.WriteLine("Test No. 4 - Passed");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Test No. 4 - An error has occured.", e);
+            }
         }
 
         [Test]
@@ -71,14 +99,25 @@ namespace Calculator
         public void Invalid()
 
         {
-            driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
-            driver.FindElement(By.XPath("//div[@id='main']/div/div/div[3]/div/div[2]")).Click();
-            driver.FindElement(By.LinkText("$$q")).Click();
-            driver.FindElement(By.LinkText("$$a")).Click();
-            driver.FindElement(By.LinkText("$$s")).Click();
-            IWebElement element = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[1]"));
-            String toolTipText = element.GetAttribute("tooltip");
-            Assert.AreEqual("Too many variables. Try defining 'q', 'a' or 's'.", toolTipText);
+            try
+            {
+
+                driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
+                driver.FindElement(By.XPath("//div[@id='main']/div/div/div[3]/div/div[2]")).Click();
+                driver.FindElement(By.LinkText("$$q")).Click();
+                driver.FindElement(By.LinkText("$$a")).Click();
+                driver.FindElement(By.LinkText("$$s")).Click();
+                IWebElement element = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[1]"));
+                String toolTipText = element.GetAttribute("tooltip");
+                Assert.AreEqual("Too many variables. Try defining 'q', 'a' or 's'.", toolTipText);
+                Console.WriteLine("Test No. 5 - Passed");
+            }
+
+            catch (Exception e)
+
+            {
+                Console.WriteLine("Test No. 5 - An error has occured.", e);
+            }
         }
 
 
@@ -89,12 +128,22 @@ namespace Calculator
         public void Delete()
 
         {
-            driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
-            driver.FindElement(By.LinkText("$$7")).Click();
-            driver.FindElement(By.LinkText("$$8")).Click();
-            driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[3]/div[2]/div[1]/div[9]/a")).Click();
-            string deleteValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[4]/div/div/span[2]/span")).Text;
-            Assert.IsTrue(deleteValue.Contains("7"));
+            try
+            {
+
+                driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
+                driver.FindElement(By.LinkText("$$7")).Click();
+                driver.FindElement(By.LinkText("$$8")).Click();
+                driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[3]/div[2]/div[1]/div[9]/a")).Click();
+                string deleteValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[4]/div/div/span[2]/span")).Text;
+                Assert.IsTrue(deleteValue.Contains("7"));
+                Console.WriteLine("Test No.6 - Passed");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Test No. 6 - An error has occured.", e);
+            }
         }
 
         [Test]
@@ -103,15 +152,23 @@ namespace Calculator
 
         public void Answer()
         {
-            driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
-            driver.FindElement(By.LinkText("$$9")).Click();
-            driver.FindElement(By.LinkText("$$÷")).Click();
-            IWebElement element = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[1]"));
-            String toolTipText3 = element.GetAttribute("tooltip");
-            Assert.AreEqual("You need a denominator for the bottom of your fraction.", toolTipText3);
-            driver.FindElement(By.LinkText("$$5")).Click();
-            string answerValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[1]/div")).Text;
-            Assert.IsTrue(answerValue.Contains("Your answers show up on this side."));
+            try
+            {
+                driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
+                driver.FindElement(By.LinkText("$$9")).Click();
+                driver.FindElement(By.LinkText("$$÷")).Click();
+                IWebElement element = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[1]"));
+                String toolTipText3 = element.GetAttribute("tooltip");
+                Assert.AreEqual("You need a denominator for the bottom of your fraction.", toolTipText3);
+                driver.FindElement(By.LinkText("$$5")).Click();
+                string answerValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[1]/div")).Text;
+                Assert.IsTrue(answerValue.Contains("Your answers show up on this side."));
+                Console.WriteLine("Test no.7-Passed");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Test No. 7 - An error has occured.", e);
+            }
         }
 
 
@@ -121,19 +178,29 @@ namespace Calculator
 
         public void Addition()
         {
-            driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
-            driver.FindElement(By.LinkText("$$7")).Click();
-            driver.FindElement(By.LinkText("$$7")).Click();
-            driver.FindElement(By.LinkText("$$+")).Click();
-            IWebElement element = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[1]"));
-            String toolTipText2 = element.GetAttribute("tooltip");
-            Assert.AreEqual("You need something on both sides of the '+' symbol.", toolTipText2);
-            driver.FindElement(By.LinkText("$$1")).Click();
-            driver.FindElement(By.LinkText("$$7")).Click();
-            driver.FindElement(By.LinkText("$$2")).Click();
-            string additionValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[2]/div")).Text;
-            Assert.IsTrue(additionValue.Contains("249"));
+            try
+            {
 
+                driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
+                driver.FindElement(By.LinkText("$$7")).Click();
+                driver.FindElement(By.LinkText("$$7")).Click();
+                driver.FindElement(By.LinkText("$$+")).Click();
+                IWebElement element = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[1]"));
+                String toolTipText2 = element.GetAttribute("tooltip");
+                Assert.AreEqual("You need something on both sides of the '+' symbol.", toolTipText2);
+                driver.FindElement(By.LinkText("$$1")).Click();
+                driver.FindElement(By.LinkText("$$7")).Click();
+                driver.FindElement(By.LinkText("$$2")).Click();
+                string additionValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[2]/div")).Text;
+                Assert.IsTrue(additionValue.Contains("249"));
+                Console.WriteLine("Test No. 8 - Passed");
+            }
+
+            catch (Exception e)
+
+            {
+                Console.WriteLine("Test No. 8 - An error has occured.", e);
+            }
         }
 
         [Test]
@@ -142,16 +209,27 @@ namespace Calculator
 
         public void Subtraction()
         {
-            driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
-            driver.FindElement(By.LinkText("$$1")).Click();
-            driver.FindElement(By.LinkText("$$−")).Click();
-            IWebElement element = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[1]"));
-            String toolTipText2 = element.GetAttribute("tooltip");
-            Assert.AreEqual("You need something on both sides of the '-' symbol.", toolTipText2);
-            driver.FindElement(By.LinkText("$$−")).Click();
-            driver.FindElement(By.LinkText("$$1")).Click();
-            string subtractValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[2]/div")).Text;
-            Assert.IsTrue(subtractValue.Contains("2"));
+            try
+            {
+
+                driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
+                driver.FindElement(By.LinkText("$$1")).Click();
+                driver.FindElement(By.LinkText("$$−")).Click();
+                IWebElement element = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[1]"));
+                String toolTipText2 = element.GetAttribute("tooltip");
+                Assert.AreEqual("You need something on both sides of the '-' symbol.", toolTipText2);
+                driver.FindElement(By.LinkText("$$−")).Click();
+                driver.FindElement(By.LinkText("$$1")).Click();
+                string subtractValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[2]/div")).Text;
+                Assert.IsTrue(subtractValue.Contains("2"));
+                Console.WriteLine("Test No. 9 - Passed");
+            }
+
+            catch (Exception e)
+
+            {
+                Console.WriteLine("Test No. 9 - An error has occured.", e);
+            }
         }
 
         [Test]
@@ -159,19 +237,30 @@ namespace Calculator
         //Basic - 10. Multiplication
 
         public void Multiplication()
+
         {
-            driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
-            driver.FindElement(By.LinkText("$$7")).Click();
-            driver.FindElement(By.LinkText("$$.")).Click();
-            driver.FindElement(By.LinkText("$$1")).Click();
-            driver.FindElement(By.LinkText("$$×")).Click();
-            IWebElement element = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[1]"));
-            String toolTipText4 = element.GetAttribute("tooltip");
-            Assert.AreEqual("You need something on both sides of the '*' symbol.", toolTipText4);
-            driver.FindElement(By.LinkText("$$5")).Click();
-            driver.FindElement(By.LinkText("$$1")).Click();
-            string multipValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[2]")).Text;
-            Assert.IsTrue(multipValue.Contains("362.1"));
+            try
+            {
+
+                driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
+                driver.FindElement(By.LinkText("$$7")).Click();
+                driver.FindElement(By.LinkText("$$.")).Click();
+                driver.FindElement(By.LinkText("$$1")).Click();
+                driver.FindElement(By.LinkText("$$×")).Click();
+                IWebElement element = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[1]"));
+                String toolTipText4 = element.GetAttribute("tooltip");
+                Assert.AreEqual("You need something on both sides of the '*' symbol.", toolTipText4);
+                driver.FindElement(By.LinkText("$$5")).Click();
+                driver.FindElement(By.LinkText("$$1")).Click();
+                string multipValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[2]")).Text;
+                Assert.IsTrue(multipValue.Contains("362.1"));
+                Console.WriteLine("Test No. 10 - Passed");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Test No. 10 - An error has occured.", e);
+            }
         }
 
         [Test]
@@ -180,15 +269,27 @@ namespace Calculator
 
         public void Fraction()
         {
-            driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
-            driver.FindElement(By.LinkText("$$1")).Click();
-            driver.FindElement(By.LinkText("$$÷")).Click();
-            IWebElement element = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[1]"));
-            String toolTipText5 = element.GetAttribute("tooltip");
-            Assert.AreEqual("You need a denominator for the bottom of your fraction.", toolTipText5);
-            driver.FindElement(By.LinkText("$$0")).Click();
-            string answerValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[2]/div")).Text;
-            Assert.IsTrue(answerValue.Contains("undefined"));
+            try
+            {
+
+                driver.Navigate().GoToUrl("https://www.desmos.com/scientific");
+                driver.FindElement(By.LinkText("$$1")).Click();
+                driver.FindElement(By.LinkText("$$÷")).Click();
+                IWebElement element = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[1]"));
+                String toolTipText5 = element.GetAttribute("tooltip");
+                Assert.AreEqual("You need a denominator for the bottom of your fraction.", toolTipText5);
+                driver.FindElement(By.LinkText("$$0")).Click();
+                string answerValue = driver.FindElement(By.XPath("/html/body/div[3]/div/div/div[1]/div[2]/div/div[5]/div[2]/div")).Text;
+                Assert.IsTrue(answerValue.Contains("undefined"));
+                Console.WriteLine("Test No. 11 - Passed");
+            }
+
+            catch (Exception e)
+
+            {
+                Console.WriteLine("Test No. 11 - An error has occured.", e);
+            }
+
         }
 
             
